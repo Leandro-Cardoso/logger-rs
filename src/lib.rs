73baa5a7999f6921e0bc7cf75daaf6a1699f31a8
Leandro Cardoso <leandrocardosodev@gmail.com>
2026-07-28 15:19:53 -0300

    Core da lib

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ pub mod entry;
 pub mod error;
 pub mod formatter;
 pub mod levels;
-// pub mod logger;
+pub mod logger;
 pub mod reader;
 pub mod storage;
 
@@ -11,4 +11,4 @@ pub use config::LoggerConfig;
 pub use entry::LogEntry;
 pub use error::LoggerError;
 pub use levels::LogLevel;
-// pub use logger::Logger;
+pub use logger::Logger;
